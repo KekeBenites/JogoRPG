e6367f8108d7b29fc0e6c36a86287cf1daf2d80f
Kleber Benites <36742090+KekeBenites@users.noreply.github.com>
2019-03-13 19:27:01 -0300

    Update
    Colocando o personagem em Struct

--- JogoTeste.cpp
+++ JogoTeste.cpp
@@ -2,12 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 
-int vidaheroi = 100, vidainimigo=10;
-int staminaheroi = 10, staminainimigo = 10;
-int especialheroi = 5, especialinimigo = 5;
+struct Personagem {
 
+int level = 1;
+int vidaheroi = 100;
+int staminaheroi = 10;
+int especialheroi = 5;
+float experieniaheroi = 1;	
+
+};
+
+int  vidainimigo=10;
+int  staminainimigo = 10;
+int  especialinimigo = 5;
+float experienciainimigo = 1;
+
+struct Personagem personagem;
 
 int main(int argc, char** argv) {
+	
+
 	
 	int op = 5;
 	void menu(int vidaheroi, int vidainimigo, int staminaheroi,int staminainimigo,int especialheroi, int especialinimigo);
@@ -17,83 +31,63 @@ int main(int argc, char** argv) {
 	int ataqueinimigo();
 	
 	do {
-		menu(vidaheroi, vidainimigo,staminaheroi,staminainimigo,especialheroi,especialinimigo);
+		menu(personagem.vidaheroi, vidainimigo,personagem.staminaheroi,staminainimigo,personagem.especialheroi,especialinimigo);
 		scanf("%i",&op);
 		system("cls"); 
 		
 		switch(op){
 			
 			case 1:
-				if (staminaheroi >=1 && vidainimigo >= 1){
+				if (personagem.staminaheroi >=1 && vidainimigo >= 1){
 					vidainimigo -= soco();
-					staminaheroi--;				
+					personagem.staminaheroi--;				
 				}
 				break;
 				
 			case 2:
-				if (staminaheroi >=2 && vidainimigo >= 1){
+				if (personagem.staminaheroi >=2 && vidainimigo >= 1){
 					vidainimigo -= chute();
-					staminaheroi -= 2;				
+					personagem.staminaheroi -= 2;				
 				}
 				
 				break;
 				
 			case 3:
 				
-				if (staminaheroi>=5 && especialheroi >=1 && vidainimigo >= 1){
+				if (personagem.staminaheroi>=5 && personagem.especialheroi >=1 && vidainimigo >= 1){
 					vidainimigo -= especial();
-					especialheroi -= 1;	
-					staminaheroi -= 5;			
+					personagem.especialheroi -= 1;	
+					personagem.staminaheroi -= 5;			
 				}
 				
 				break;
 				
 			case 4:
 				
-				if (staminaheroi >= 20)
-					staminaheroi = 20;
+				if (personagem.staminaheroi >= 20)
+					personagem.staminaheroi = 20;
 				else {
-					staminaheroi += 5;
-					if (staminaheroi>20){
-						staminaheroi = 20;
+					personagem.staminaheroi += 5;
+					if (personagem.staminaheroi>20){
+						personagem.staminaheroi = 20;
 					}
 					
 				}
-				
-				
-				
+	
 			default:
 				
 				break;
 			
 		}
 		
-	vidaheroi -= ataqueinimigo();	
+	personagem.vidaheroi -= ataqueinimigo();	
 		
 	}while (op!=0);
 	
 	return 0;
 }
 
-void menu(int vidaheroi, int vidainimigo, int staminaheroi,int staminainimigo,int especialheroi, int especialinimigo){
-	
-	printf("\n");
-	printf(" ============ HEROI ============ / ============ INIMIGO LVL =============\n ");
-       printf("=\t Vida = %i\t \t / = \t Vida = %i\t\t\t=\n ",vidaheroi,vidainimigo);	
-	printf("=\t Stamina = %i\t \t / = \t Stamina = %i\t\t\t=\n ",staminaheroi,staminainimigo);		
-   printf("=\t Especial = %i\t \t / = \t Especial =  %i\t\t\t=\n ",especialheroi,especialinimigo);	
-	printf("=============================== / ======================================\n\n ");	
-		
-	printf("========= ACAO DO HEROI =========\n");
-	printf(" 1 - Soco (1 Dano / 1 Stamina)\n");
-	printf(" 2 - Chute (2 Dano / 2 Stamina)\n");
-	printf(" 3 - Especial (5 Dano / 5 Stamina)\n");
-	printf(" 4 - Descancar (Perde 1 turno)\n");
-	printf(" ================================\n");
-	printf("O que vc quer fazer?\n\n");
-	
-	
-	}
+
 	
 	int soco(){
 		int soco = 1;
